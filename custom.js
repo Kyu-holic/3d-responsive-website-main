b977fd36405a0c1edae223f747dfb3f80ed9ed7e
kyuholic <khhan1990@hanmail.net>
2022-07-05 08:13:40 +0900

    PC레이아웃 섹션 상세 퍼블리싱(Plan)-#04(Slick.js 스타일 변경) 완성

--- custom.js
+++ custom.js
@@ -1,10 +1,12 @@
+// Slick.js : history
 $(function () {
-  // slick.js
   $(".history-slider").slick({
     dots: true,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
   });
 });
+
+// slick.js : Project
