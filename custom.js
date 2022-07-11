db3b66e6e54419b04a4e7077bdfd82520699bcfb
kyuholic <khhan1990@hanmail.net>
2022-07-12 08:16:43 +0900

    PC레이아웃 섹션 상세 퍼블리싱(Project)#06(Slick.js이미지 슬라이더) 완성

--- custom.js
+++ custom.js
@@ -7,6 +7,14 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 2,
   });
+  // slick.js : Project
+  $(".project-photo").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  });
 });
-
-// slick.js : Project
